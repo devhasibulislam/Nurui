bc5160b41eb5b92e6eb971d08a4b53b1fbc3a9cb
hasibulislam999 <hasib143sl@gmail.com>
2023-01-06 10:12:49 +0600

    tag service init

---
+++ server/services/tag.service.js
@@ -0,0 +1,82 @@
+const Tag = require("../models/Tag");
+const imageRemover = require("../utils/imageRemover.util");
+
+const selectors = "_id name email avatar role status";
+
+exports.postNewTag = async (data) => {
+  const result = await Tag.create(data);
+  return result;
+};
+
+exports.getAllTags = async (query) => {
+  const result = await Tag.find(query)
+    .populate([
+      {
+        path: "creator",
+        select: selectors,
+      },
+      {
+        path: "posts",
+        select:
+          "_id thumbnail globalTag tags likes creator watches comments description title readTime",
+        populate: [
+          {
+            path: "creator",
+            select: selectors,
+          },
+          {
+            path: "tags",
+            select: "_id title thumbnail",
+            populate: {
+              path: "creator",
+              select: selectors,
+            },
+          },
+        ],
+      },
+    ])
+    .sort("-updatedAt");
+  return result;
+};
+
+exports.getTag = async ({ id }) => {
+  const result = await Tag.findById(id).populate([
+    {
+      path: "creator",
+      select: selectors,
+    },
+    {
+      path: "posts",
+      select:
+        "_id thumbnail globalTag tags creator watches comments description title readTime",
+      populate: [
+        {
+          path: "creator",
+          select: selectors,
+        },
+        {
+          path: "tags",
+          select: "_id title thumbnail",
+          populate: {
+            path: "creator",
+            select: selectors,
+          },
+        },
+      ],
+    },
+  ]);
+  return result;
+};
+
+exports.updateTag = async (id, data) => {
+  const result = await Tag.findByIdAndUpdate(id, data, {
+    runValidators: false,
+  });
+  return result;
+};
+
+exports.deleteTag = async ({ id }) => {
+  const result = await Tag.findByIdAndDelete(id);
+  await imageRemover(result?.thumbnail?.public_id);
+  return result;
+};
