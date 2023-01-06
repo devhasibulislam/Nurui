f5ff638d2c5a0a22ce6e978f393c5ef170fbe31d
hasibulislam999 <hasib143sl@gmail.com>
2023-01-06 10:02:18 +0600

    dashboard post card init

---
+++ client/src/components/dashboard/PostCard.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { BsPencilSquare } from "react-icons/bs";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { Link, useNavigate } from "react-router-dom";
+import { useDeletePostMutation } from "../../features/post/postApi";
+
+const PostCard = ({ post }) => {
+  const [deletePost, { isLoading }] = useDeletePostMutation();
+  const navigate = useNavigate();
+
+  return (
+    <section
+      className="lg:p-12 md:p-8 p-4 rounded-xl bg-no-repeat bg-center relative z-0 hover:-translate-y-1 hover:-translate-x-1 transition-all duration-200"
+      style={{ backgroundImage: `url(${post?.thumbnail?.url})` }}
+    >
+      <div className="absolute w-full h-full top-0 left-0 bg-black rounded-xl opacity-50 -z-10" />
+      {post?.globalTag !== "none" && (
+        <div className="absolute top-4 left-4 flex flex-row gap-x-3">
+          <div
+            style={{ paddingTop: "0.1em", paddingBottom: "0.1rem" }}
+            className="text-xs px-3 rounded-full bg-purple-200 text-purple-800"
+          >
+            {post?.globalTag}
+          </div>
+        </div>
+      )}
+      <div className="absolute top-4 right-4 flex flex-row gap-x-3">
+        {isLoading ? (
+          <span className="text-white">Loading...</span>
+        ) : (
+          <>
+            <BsPencilSquare
+              onClick={() => navigate(`/dashboard/post-list/${post?._id}`)}
+              className="text-black text-3xl bg-white hover:bg-green-500 hover:text-white transition-all delay-200 rounded-lg p-1 cursor-pointer"
+            />
+            <RiDeleteBin2Line
+              onClick={() => deletePost(post?._id)}
+              className="text-black text-3xl bg-white hover:bg-red-500 hover:text-white transition-all delay-200 rounded-lg p-1 cursor-pointer"
+            />
+          </>
+        )}
+      </div>
+      <article className="z-10 text-white flex flex-col gap-y-4">
+        <h2 className="text-2xl font-medium text-ellipsis overflow-hidden whitespace-nowrap">
+          <Link to={`/post/${post?._id}`}>{post?.title}</Link>
+        </h2>
+        <p className="text-sm text-ellipsis overflow-hidden whitespace-nowrap">
+          {post?.description}
+        </p>
+      </article>
+      <div className="flex flex-row flex-wrap gap-2 mt-8">
+        {post?.tags?.map((tag, index) => (
+          <div
+            style={{ paddingTop: "0.1em", paddingBottom: "0.1rem" }}
+            className={`text-xs px-3 rounded-full ${
+              index % 2 === 0
+                ? "bg-red-200 text-red-800"
+                : "bg-indigo-200 text-indigo-800"
+            }`}
+          >
+            {tag?.title}
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-4 right-4 flex flex-row gap-x-3">
+        <div
+          style={{ paddingTop: "0.1em", paddingBottom: "0.1rem" }}
+          className="text-xs px-3 rounded bg-slate-200 text-slate-800"
+        >
+          Read {post?.readTime} min
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PostCard;
