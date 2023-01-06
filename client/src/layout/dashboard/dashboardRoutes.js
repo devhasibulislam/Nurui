bb68bd2decd03e70565c6404c6d341560feec18f
hasibulislam999 <hasib143sl@gmail.com>
2023-01-06 10:05:38 +0600

    layout sidebar routes init

---
+++ client/src/layout/dashboard/dashboardRoutes.js
@@ -0,0 +1,29 @@
+import { BsTagsFill, BsFillSignpost2Fill } from "react-icons/bs";
+import { HiClipboardList } from "react-icons/hi";
+import { RiFileListFill } from "react-icons/ri";
+
+const iconStyle = "lg:h-4 md:h-10 h-8 lg:w-4 md:w-10 w-8 lg:mx-0 md:mx-auto";
+const dashboardRoutes = [
+  {
+    icon: <BsTagsFill className={iconStyle} />,
+    name: "Add Tag",
+    anchor: "add-tag",
+  },
+  {
+    icon: <HiClipboardList className={iconStyle} />,
+    name: "Tag List",
+    anchor: "tag-list",
+  },
+  {
+    icon: <BsFillSignpost2Fill className={iconStyle} />,
+    name: "Add Post",
+    anchor: "add-post",
+  },
+  {
+    icon: <RiFileListFill className={iconStyle} />,
+    name: "Post List",
+    anchor: "post-list",
+  },
+];
+
+export default dashboardRoutes;
