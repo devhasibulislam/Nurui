f3e61cd88f6c1c331109359b42f9588656176756
hasibulislam999 <hasib143sl@gmail.com>
2023-01-07 13:35:33 +0600

    init admin all post route

---
+++ client/src/page/dashboard/post/AllPosts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PostCard from "../../../components/dashboard/PostCard";
+import Loading from "../../../components/shared/Loading";
+import { useGetAllPostsQuery } from "../../../features/post/postApi";
+
+const AllPosts = () => {
+  const { data, isLoading } = useGetAllPostsQuery();
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+      {data?.data?.map((post) => (
+        <PostCard key={post._id} post={post} />
+      ))}
+    </section>
+  );
+};
+
+export default AllPosts;
