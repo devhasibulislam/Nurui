857e0f3149fb7617d2b0c965d31e731b0fd4f1e8
hasibulislam999 <hasib143sl@gmail.com>
2023-01-07 13:35:58 +0600

    modify post list based on user

--- client/src/page/dashboard/post/PostList.js
+++ client/src/page/dashboard/post/PostList.js
@@ -1,16 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import PostCard from "../../../components/dashboard/PostCard";
 import Loading from "../../../components/shared/Loading";
-import { useGetAllPostsQuery } from "../../../features/post/postApi";
 
 const PostList = () => {
-  const { data, isLoading } = useGetAllPostsQuery();
+  const { userCredentials, isLoading } = useSelector((state) => state.auth);
 
   return isLoading ? (
     <Loading />
   ) : (
     <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-      {data?.data?.map((post) => (
+      {userCredentials?.posts?.map((post) => (
         <PostCard key={post._id} post={post} />
       ))}
     </section>
