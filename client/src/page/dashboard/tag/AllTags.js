b4101ca50448f90b61188a47dc7844721d56192a
hasibulislam999 <hasib143sl@gmail.com>
2023-01-07 13:35:47 +0600

    init admin all tag route

---
+++ client/src/page/dashboard/tag/AllTags.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TagCard from "../../../components/dashboard/TagCard";
+import Loading from "../../../components/shared/Loading";
+import { useGetAllTagsQuery } from "../../../features/tag/tagApi";
+
+const AllTags = () => {
+  const { data, isLoading } = useGetAllTagsQuery();
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+      {data?.data?.map((tag) => (
+        <TagCard key={tag._id} tag={tag} />
+      ))}
+    </section>
+  );
+};
+
+export default AllTags;
