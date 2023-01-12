b67d1a3569d08baf0b02ddec3f277eda564b1aa5
hasibulislam999 <hasib143sl@gmail.com>
2023-01-12 23:29:48 +0600

    outlet block fix

--- client/src/layout/dashboard/Dashboard.js
+++ client/src/layout/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <section className="grid grid-cols-12 p-3 gap-3 h-screen w-screen">
       <Sidebar />
-      <div className="col-span-10 w-full bg-gray-100 rounded-lg p-4">
+      <div className="col-span-10 w-full bg-gray-100 rounded-lg p-4 overflow-x-hidden">
         <Outlet />
       </div>
     </section>
